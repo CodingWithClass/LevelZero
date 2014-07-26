710824bba65d7ab6ab37c0ee83e3932ac8de3a98
CodingWithClass <zeb@classcoder.com>
2014-07-26 08:42:13 -0400

    Forgot install utility

---
+++ install-utility.js
@@ -0,0 +1,16 @@
+// Installation Utility
+alert("Welcome to Installation Utility.");
+var bootsector = prompt("Please paste the contents of \"bootsector\":", " ");
+console.debug(bootsector);
+if (bootsector.length > 512) {
+  throw "Long bootloader";
+}
+var diskinfo = prompt("Please paste the contents of \"OS\"", " ");
+console.debug(diskinfo);
+for (i = 0; i <= bootsector.length; i++) {
+  diskWrite(0, i, bootsector[i]);
+}
+for (i = 0; i <= diskinfo.length; i++) {
+  diskWrite(0, i, diskinfo[i]);
+}
+alert("Complete!");
